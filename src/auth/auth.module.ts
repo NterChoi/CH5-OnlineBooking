c8fa13043f4e38672309f4f4be1ea0876e144baa
NterChoi <choi16289@naver.com>
2024-10-22 15:07:25 +0900

    add :회원가입 및 회원가입 시 포인트 지급 트랜잭션 처리

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule.register({defaultStrategy: 'jwt', session: false}),
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET_KEY'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [JwtStrategy]
+})
 export class AuthModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { SignupDto } from './dto/signup.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { EntityManager, Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { Role } from './types/userRole.type';
+import { Point } from '../point/entities/point.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: SignupDto) {
-    return 'This action adds a new user';
+  constructor(@InjectEntityManager() private readonly entityManager: EntityManager,
+              @InjectRepository(User)
+              private userRepository: Repository<User>,
+              @InjectRepository(Point)
+              private pointRepository: Repository<Point>,
+              private readonly jwtService: JwtService) {
+  }
+
+  async create(createUserDto: SignupDto): Promise<User> {
+    return await this.entityManager.transaction(async (manager) => {
+      const existingUser = await this.findByEmail(createUserDto.email);
+      if (existingUser) {
+        throw new ConflictException('이미 사용중인 이메일입니다.');
+      }
+      // 1. 유저 생성
+      const user = await manager.getRepository(User).save({
+        email: createUserDto.email,
+        password: createUserDto.password,
+        nickname: createUserDto.nickname,
+      });
+      // 2. 포인트 지급
+      await this.pointRepository.save({
+        userId: user.id,
+        value: 1000000,
+        description: '가입 지급 포인트',
+      });
+      return user;
+    });
   }
 
   findAll() {
@@ -22,5 +54,10 @@ export class UserService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  private async findByEmail(email: string) {
+    return await this.userRepository.findOneBy({ email });
+
   }
 }
