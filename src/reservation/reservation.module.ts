b4623e7059e9ab5b320cff17b615cd5278d94bf4
NterChoi <choi16289@naver.com>
2024-10-26 18:14:10 +0900

    update : app.module, reservation.module에 엔티티 추가

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -7,9 +7,12 @@ import { Theater } from '../theater/theater.entity';
 import { Box } from '../box/box.entity';
 import { Schedule } from '../schedule/entities/schedule.entity';
 import { Seat } from '../seat/seat.entity';
+import { ReservedSeat } from '../reservedSeat/reservedSeat.entity';
+import { Point } from '../point/entities/point.entity';
+import { Reservation } from './entities/reservation.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Show, Theater, Box, Schedule, Seat]),],
+  imports: [TypeOrmModule.forFeature([Schedule, Seat, ReservedSeat, Point, Reservation]),],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
